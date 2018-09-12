ea9ce5704ad8858a8d97c89244b3020ebe6d0a26
Alexey Ryzhkov <ryzhkov@scand.com>
2018-09-12 15:41:49 +0300

    feat: implement feature getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  let triangles = [];
+
+  for(let i = 0; i < preferences.length; i ++){
+    let triangle = [ preferences[i], preferences[[preferences[i] - 1]], preferences[preferences[preferences[i] - 1]- 1]];
+    if(triangle[2]  === (i + 1)){
+      if((triangles.map(x => x[0]).indexOf(triangle.sort()[0]) === -1 )
+      && (triangles.map(x => x[1]).indexOf(triangle.sort()[1]) === -1 )
+      && (triangles.map(x => x[2]).indexOf(triangle.sort()[2]) === -1 )
+      && triangle[0] !== triangle[1]){
+        triangles.push(triangle.sort());
+        ++count;
+      }
+    }
+  }
+  return count;
 };
